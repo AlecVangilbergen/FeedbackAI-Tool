cbeb67c0adfaa297629c8adbcd27b8d5ac12af2f
Sandeep Singh <sandeep159@outlook.com>
2024-04-16 16:01:57 +0200

    Add link in navbar for UploadCourseDocumentsPage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,6 +31,7 @@ const Navbar: React.FC = () => {
                         <>
                             <Link to="/assignment" className="text-white hover:text-primary-light transition-colors duration-200">Create Assignment</Link>
                             <Link to="/registercourse" className="text-white hover:text-primary-light transition-colors duration-200">Register Course</Link>
+                            <Link to="/uploadcoursedocuments" className="text-white hover:text-primary-light transition-colors duration-200">Upload Documents</Link>
                             <Link to="/courses" className="text-white hover:text-primary-light transition-colors duration-200">Course Overview</Link>
                             <Link to="/assignments" className="text-white hover:text-primary-light transition-colors duration-200">Assignment Overview</Link>
                             <Link to="/submissions" className="text-white hover:text-primary-light transition-colors duration-200">Submission Overview</Link>
